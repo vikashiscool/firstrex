db33bf18109297aa14d0406c6ae7223a81478a20
vikashiscool <vikash_parekh@yahoo.com>
2016-02-05 14:35:04 -0800

    Fix styling. Attempt to chainge index to framework and login to default landing page. Incorporate Google Maps

--- www/js/app.js
+++ www/js/app.js
@@ -25,9 +25,8 @@ app.config(function ($stateProvider, $urlRouterProvider){
   // Ionic uses AngularUI Router, which uses the concept of states.
   // More info can be found here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
-
+  //stateProvider determines which templates are injected where ion-nav-view is located in parent template (index.html)
   // set up an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
@@ -36,37 +35,174 @@ app.config(function ($stateProvider, $urlRouterProvider){
   })
 
   //login route
-    .state('index', {
-    url: "/index",
-    templateUrl: "index.html",
+    .state('home', {
+    url: "/login",
+    views: {
+      login: {
+        templateUrl: "login.html"
+      }
+    },
     controller: 'LoginCtrl'
       })
 
   // prequalification form
     .state('request', {
       url: "/request",
-      templateUrl: "request.html",
+      views: {
+        login: {
+          templateUrl: "request.html"
+      }
+    },
       controller: "RequestCtrl"
     })
 
-   $urlRouterProvider.otherwise("/tab/login");
+   $urlRouterProvider.otherwise("templates/login");
 
 })
 
 //HTTP requests with Ionic from: https://blog.nraboy.com/2014/08/make-http-requests-android-ios-ionicframework/
 
-app.controller('LoginCtrl', function($scope, $http/*, LoginService, $ionicPopup, $state*/) {
+app.controller('LoginCtrl', function($scope, $http, /*, LoginService,*/ $ionicPopup, $state) {
   // $scope.data = {};
   $scope.getData = function() {
     $http.get("http://portal1rexcom-stage.elasticbeanstalk.com/authorize/signin", { params: { "email": "admin@1rex.com", "password": "shareable" } })
         .success(function(data) {
           $scope.email = data.email;
           $scope.password = data.password;
+          $ionicViewService.nextViewOptions({
+            disableAnimate: true,
+            disableBack: true
+          });
         })
         .error(function(data) {
           alert("ERROR");
         });
   }
+
+// validate login --> log you into server
+
+ var loginInfo = {email:'admin@1rex.com', password:'shareable'}
+    $http.post({"http://portal1rexcom-stage.elasticbeanstalk.com/authorize/signin", loginInfo})
+      .success(function(data){
+        $state.go('request');
+    })
+      .error(function(data) {
+        var alertPopup = $ionicPopup.alert({
+          title: 'Login failed',
+          template: "Please check your credentials"
+        })
+      })
+ 
+   //store json and read out phone number
+
+  var json = 
+    $http({
+    var email = angular.fromJson(json)["email"],
+    var phone = angular.fromJson(json)["phone"],  
+    url: "http://portal1rexcom-stage.elasticbeanstalk.com/user/search/:email",
+    method: "GET",
+    params: {email: 'email', password: 'phone'}
+    })
+
+    
+})
+
+app.controller('RequestCtrl', function($scope, $location, $ionicViewService) {
+  if(window.localStorage.getItem("password") === "undefined" || window.localStorage.getItem("password") === null) {
+      $ionicViewService.nextViewOptions({
+        disableAnimate: true,
+        disableBack: true
+        });
+      $location.path("/login");
+  }
+});
+
+////////////////////////////
+//Google Map Functinoality//
+///////////////////////////
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -33.8688, lng: 151.2195},
+    zoom: 13
+  });
+  var input = /** @type {!HTMLInputElement} */(
+      document.getElementById('pac-input'));
+
+  var types = document.getElementById('type-selector');
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+
+  autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);  // Why 17? Because it looks good.
+    }
+    marker.setIcon(/** @type {google.maps.Icon} */({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+  });
+
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  function setupClickListener(id, types) {
+    var radioButton = document.getElementById(id);
+    radioButton.addEventListener('click', function() {
+      autocomplete.setTypes(types);
+    });
+  }
+
+  setupClickListener('changetype-all', []);
+  setupClickListener('changetype-address', ['address']);
+  setupClickListener('changetype-establishment', ['establishment']);
+  setupClickListener('changetype-geocode', ['geocode']);
+}
+
+
+
+
+
+
+
+
 
 
   // $scope.login = function() {
@@ -81,31 +217,12 @@ app.controller('LoginCtrl', function($scope, $http/*, LoginService, $ionicPopup,
   // }
 
 
-// validate login --> log you into server
-
- var loginInfo = {email:'admin@1rex.com', password:'shareable'}
-    $http({
-    url: "http://portal1rexcom-stage.elasticbeanstalk.com/authorize/signin",
-    method: "POST",
-    params: loginInfo
-    })
- 
-   //store json and read out phone number
-
-  var json = 
-    $http({
-    var email = angular.fromJson(json)["email"],
-    var phone = angular.fromJson(json)["phone"],  
-    url: "http://portal1rexcom-stage.elasticbeanstalk.com/user/search/:email",
-    method: "GET",
-    params: {email: 'email', password: 'phone'}
-    })
 
     // store these and check to see if the email in the form matches the information the data that's returned from the API. If match, then redirect to prequal page. Else error. 
 
  
 
-})
+
 
 
 
@@ -146,7 +263,3 @@ app.controller('LoginCtrl', function($scope, $http/*, LoginService, $ionicPopup,
     // }, 15000);  
 // })
 
-
-app.controller('RequestCtrl', function($scope){
-
-});
